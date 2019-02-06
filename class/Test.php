d904809006183801047bc88eeee5c7101fb40bfc
Paul saighi <paulsaighi@hotmail.com>
2019-02-06 21:34:01 +0100

    Alors, j'ai enlevé page_login, le chargement du fichier se fait directement sur Test.php, le fichier est envoyer grace à un formulaire comportant un attribut  enctype='multipart/form-data', puis est charger sur le serveur grace à move_uploaded_file($name, $tmp_name), une fois cela fait on peut le manipuler sur le serveur indépendamment de l'utilisateur et hop on l'interprète et on l'affiche :))))

--- class/Test.php
+++ class/Test.php
@@ -1,14 +1,48 @@
 <?php
 require_once ('Tools.php');
 
-$proteins= new ConverterfromTxt('fichier_proteines.txt');
-// affiche les infos des protéines
-/*foreach (preg_split("@(\s)+@",$proteins->getInfoProtein()) as $prot){
-	echo $prot;
-}*/
+if (isset($_FILES["fichier"])) {
+    $tmp_name = $_FILES["fichier"]["tmp_name"];
+	$name = $_FILES["fichier"]["name"];
+	move_uploaded_file($name, $tmp_name);
+	$proteins= new ConverterfromTxt($_FILES["fichier"]["tmp_name"]);
 
-foreach ($proteins->getListOfProteins() as $prot){
-	SVG::show($prot);
-	}
+	// affiche les infos des protéines
+	/*foreach (preg_split("@(\s)+@",$proteins->getInfoProtein()) as $prot){
+		echo $prot;
+	}*/
+
+	foreach ($proteins->getListOfProteins() as $prot){
+		SVG::show($prot);
+		}
+
+	
+}else{
+
+	function debut_html($title) {
+		return
+		  "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML Basic 1.1//EN'
+			  'http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd'>\n" .
+		  "<html xmlns='http://www.w3.org/1999/xhtml' lang='fr'>\n" .
+		  "<head>\n" .
+		  "<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />\n" .
+		  "<title>" . 
+		  $title .
+		  "</title>\n" .
+		  "</head>\n";
+	  }
+	
+	$title="hello";
+	
+	$body =  "<body>\n$title" . 
+				"<form action='Test.php' method='post' enctype='multipart/form-data'><fieldset>\n" .
+				"<label for id='nom'>Rentrez votre fichier ;) : </label>\n" .
+				"<input type ='file' id='fichier' name='fichier' />\n" .
+				"<input type ='submit' id='submit' name='fichier' />\n" .
+				"</fieldset></form></body></html>\n";
+	echo debut_html($title) . $body;
+	
+
+}
+
 ?>
-
