c1a4e9319d9b5646c1bd085c73ad4fb9b70e2ec4
Saighi <paulsaighi@hotmail.com>
2019-02-11 12:23:58 +0100

    OK, le checkbox lors du form permet de choisir le format que l'on veut, on envoie juste en poste le résultat de la checkbox et en fonction on envoie true ou false à l'objet SVG, l'objet affichera respectivement une forme contractée (à l'échelle) ou une forma non contractée, le mieux serait de faire tt ça ac du java script btw

--- class/Test.php
+++ class/Test.php
@@ -1,44 +1,44 @@
 <?php
 require_once ('Tools.php');
 
-if (isset($_FILES["fichier"])) {
+if (isset($_FILES["fichier"]) ) {
     $tmp_name = $_FILES["fichier"]["tmp_name"];
 	$name = $_FILES["fichier"]["name"];
 	move_uploaded_file($name, $tmp_name);
 	$proteins= new ConverterfromTxt($_FILES["fichier"]["tmp_name"]);
+	$title="Protéines affichage";
+
 
 	// affiche les infos des protéines
 	/*foreach (preg_split("@(\s)+@",$proteins->getInfoProtein()) as $prot){
 		echo $prot;
 	}*/
-
+	echo debut_html($title);
+	echo "<body>\n";
 	foreach ($proteins->getListOfProteins() as $prot){
-		SVG::show($prot,false);
+		
+		if (isset($_POST['miseAEchelle'])){
+			SVG::show($prot,true);
 		}
+		else{
+			SVG::show($prot,false);
+		}
+
+		}
+	echo "</body></html>";
 
 	
 }else{
-
-	function debut_html($title) {
-		return
-		  "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML Basic 1.1//EN'
-			  'http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd'>\n" .
-		  "<html xmlns='http://www.w3.org/1999/xhtml' lang='fr'>\n" .
-		  "<head>\n" .
-		  "<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />\n" .
-		  "<title>" . 
-		  $title .
-		  "</title>\n" .
-		  "</head>\n";
-	  }
 	
-	$title="hello";
+	$title="Protéines formulaire";
 	
 	$body =  "<body>" . 
 				"<form action='Test.php' method='post' enctype='multipart/form-data'><fieldset>\n" .
 				"<label for id='nom'>Rentrez votre fichier ;) : </label>\n" .
 				"<input type ='file' id='fichier' name='fichier' />\n" .
-				"<input type ='submit' id='submit' name='fichier' />\n" .
+				"<label for id='nom2'>Mise à l'echelle : </label>\n" .
+				"<input type ='checkbox' id='miseAEchelle' name='miseAEchelle' />\n" .
+				"<input type ='submit' id='submit' name='submit' />\n" .
 				"</fieldset></form></body></html>\n";
 	echo debut_html($title) . $body;
 	
