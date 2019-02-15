b1429122458ebbb8596e0404f2f7d85b68ce80b5
saighi paul <paulsaighi@hotmail.com>
2019-02-15 17:39:09 +0100

    commit

--- class/Test.php
+++ class/Test.php
@@ -1,12 +1,12 @@
 <?php
-require_once ('Tools.php');
+require_once('Tools.php');
 
-if (isset($_FILES["fichier"]) ) {
-    $tmp_name = $_FILES["fichier"]["tmp_name"];
+if (isset($_FILES["fichier"])) {
+	$tmp_name = $_FILES["fichier"]["tmp_name"];
 	$name = $_FILES["fichier"]["name"];
 	move_uploaded_file($name, $tmp_name);
-	$proteins= new ConverterfromTxt($_FILES["fichier"]["tmp_name"]);
-	$title="Protéines affichage";
+	$proteins = new ConverterfromTxt($_FILES["fichier"]["tmp_name"]);
+	$title = "Protéines affichage";
 
 
 	// affiche les infos des protéines
@@ -15,33 +15,32 @@ if (isset($_FILES["fichier"]) ) {
 	}*/
 	echo debut_html($title);
 	echo "<body>\n";
-	foreach ($proteins->getListOfProteins() as $prot){
-		
-		if (isset($_POST['miseAEchelle'])){
-			SVG::show($prot,true);
-		}
-		else{
-			SVG::show($prot,false);
+	foreach ($proteins->getListOfProteins() as $prot) {
+
+		if (isset($_POST['miseAEchelle'])) {
+			SVG::show($prot, true);
+		} else {
+			SVG::show($prot, false);
 		}
 
-		}
+	}
 	echo "</body></html>";
 
-	
-}else{
-	
-	$title="Protéines formulaire";
-	
-	$body =  "<body>" . 
-				"<form action='Test.php' method='post' enctype='multipart/form-data'><fieldset>\n" .
-				"<label for id='nom'>Rentrez votre fichier ;) : </label>\n" .
-				"<input type ='file' id='fichier' name='fichier' />\n" .
-				"<label for id='nom2'>Mise à l'echelle : </label>\n" .
-				"<input type ='checkbox' id='miseAEchelle' name='miseAEchelle' />\n" .
-				"<input type ='submit' id='submit' name='submit' />\n" .
-				"</fieldset></form></body></html>\n";
+
+} else {
+
+	$title = "Protéines formulaire";
+
+	$body = "<body>" .
+		"<form action='Test.php' method='post' enctype='multipart/form-data'><fieldset>\n" .
+		"<label for id='nom'>Rentrez votre fichier ;) : </label>\n" .
+		"<input type ='file' id='fichier' name='fichier' />\n" .
+		"<label for id='nom2'>Mise à l'echelle : </label>\n" .
+		"<input type ='checkbox' id='miseAEchelle' name='miseAEchelle' />\n" .
+		"<input type ='submit' id='submit' name='submit' />\n" .
+		"</fieldset></form></body></html>\n";
 	echo debut_html($title) . $body;
-	
+
 
 }
 
