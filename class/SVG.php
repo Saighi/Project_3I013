0cbe63e03f03a504f9f31e15247e659887758370
Paul saighi <paulsaighi@hotmail.com>
2019-02-21 00:39:33 +0100

    Il n'y a qu'un seul rectangle par domaine qui peut avoir plusieurs couleurs, j'ai utilisé la méthode décrite ici : https://stackoverflow.com/questions/17053043/create-a-svg-rectangle-filled-with-different-background-color-by-percentage-or-p

--- class/SVG.php
+++ class/SVG.php
@@ -20,9 +20,10 @@ abstract class SVG
 			$taille = $length + 10;
 		}
 
-		$svg = '<tr><th>.' . $proteine->getId() . '</th><td><svg height="60" width="' . $taille . '">
-			<line x1="10" y1="30" x2="' . ($taille - 10) . '" y2="30" style="stroke:black;stroke-width:2"/>';
-		$svg .= "<rect width='" . $taille . "' height='60' style='fill:rgb(0,0,0);stroke-width:3' fill-opacity='0.1'/>";
+		$svg = '<tr><th>.' . $proteine->getId() . "</th>\n"
+		."<td><svg height='60' width='" . $taille . "'>\n"
+		."<line x1='10' y1='30' x2='" . ($taille - 10)."' y2='30' style='stroke:black;stroke-width:2'/>\n";
+		$svg .= "<rect width='" . $taille . "' height='60' style='fill:rgb(0,0,0);stroke-width:3' fill-opacity='0.1'/>\n";
 
 		if ($miseAEchelle) {
 			for ($i = 150; $i < $length; $i += 150) {
@@ -42,29 +43,29 @@ abstract class SVG
 
 			$color = substr($domain->getId(), 2);
 			$color .= '0';
-			$color = $colorFile[($domainParts[$domain->getID()]['random']*$color)%148];
+			$color = $colorFile[($domainParts[$domain->getId()]['random']*$color)%148];
 			
 			$lengthDomain = ($domainLast - $domainFirst);
-			$nbPart=$domainParts[$domain->getID()]['nbParts'];
+			$nbPart=$domainParts[$domain->getId()]['nbParts'];
 
-			$svg .= '<rect x="' . $domainFirst
-					. '" y="0" width="' . $lengthDomain
-					. '" height="60" style="fill:' . ($color) . ';stroke:black;stroke-width:0.5;fill-opacity: .5"></rect>';
+			$svg .= "<rect x='" . $domainFirst
+					. "' y='0' width='" . $lengthDomain
+					. "' height='60' style='fill:" . ($color) . ";stroke:black;stroke-width:0.5;fill-opacity: .5'></rect>\n";
 					
-		
+			$fillPercentage= round(100/$nbPart);
+			//on définit des "linearGradient" qui permettent à la base de faire des dégradés, dans notre cas
+			//cela permet d'afficher plusieurs couleurs pour un rectangle
+			$svg.= "<defs>\n<linearGradient id='MyGradient".$domain->getId()."' x2='100%' y2='0%'>\n";
 			for($i=0;$i<$nbPart;$i++) {
-				if($i==0 || $i==$nbPart-1) {
-					$rx="rx=\"10\"";
-				}
-				else
-				{
-					$rx="rx=\"4\"";
-				}
-				$svg .= '<rect x="' . ($domainFirst+($lengthDomain/$nbPart)*$i)
-					. '" y="15" width="' . ($lengthDomain/$nbPart)
-					. '" height="30"  '.$rx.' style="fill:' . $colorFile[(substr($domain->getId(), 2)*($i+1)*$domainParts[$domain->getID()]['random'])%148] . ';stroke:black;stroke-width:2"><title>' . $domain->getId()
-					. '</title></rect>';
+				$svg.="<stop offset='".$fillPercentage."%' stop-color='".$colorFile[(substr($domain->getId(), 2)*($i+1)*$domainParts[$domain->getID()]['random'])%148]."' />\n";
 			}
+			$svg.= "</linearGradient>\n</defs>\n";
+
+			//après avoir défini notre linear gradient on fait appel à lui dans la définition de notre rectangle par le biais de fill =fill="url(#MyGradient'.$domain->getId().')"
+			$svg .= '<rect x="' . $domainFirst
+					. '" y="15" width="' . $lengthDomain
+					. '" height="30"  rx="10" fill="url(#MyGradient'.$domain->getId().')" stroke="black" stroke-width="2"><title>' . $domain->getId()
+					. '</title></rect>';
 			
             //x domaine commence
             //width (domaine fini - domaine commence)
--- svg.php
+++ svg.php
@@ -1,27 +1,14 @@
-<svg height="50" width="1310">
- 
-  <rect x="39" y="0" width="37" height="40" rx="15" />
-            //x domaine commence
-            /width (domaine fini - domaine commence)
-  
-<rect x="305" y="0" width="37" height="40" rx="15" />
+<svg xmlns="http://www.w3.org/2000/svg">
 
-<rect x="805" y="0" width="160" height="40" rx="15" />
-<rect x="1000" y="0" width="180" height="40" rx="15" />
+    <defs>
+      <linearGradient id="MyGradient" x2="0%" y2="100%">
+        <stop offset="33%" stop-color="white" />
+        <stop offset="33%" stop-color="#FF6" />
+        <stop offset="66%" stop-color="#FF6" />
+        <stop offset="66%" stop-color="#F60" />
+      </linearGradient>
+    </defs>
 
- <line x1="0" y1="20" x2="1300" y2="20" style="stroke:rgb(255,0,0);stroke-width:2" />
-</svg>
-<svg height="50" width="1310">
- 
-  <rect x="39" y="0" width="37" height="40" rx="15" />
-            //x domaine commence
-            /width (domaine fini - domaine commence)
-  
-<rect x="305" y="0" width="37" height="40" rx="15" />
-
-<rect x="805" y="0" width="160" height="40" rx="15" />
-<rect x="1000" y="0" width="180" height="40" rx="15" />
-
- <line x1="0" y1="20" x2="1300" y2="20" style="stroke:rgb(255,0,0);stroke-width:2" />
- //x2 taille protéine
+    <rect fill="url(#MyGradient)" stroke="black" stroke-width="5"  
+          x="100" y="100" width="200" height="600"/>
 </svg>
