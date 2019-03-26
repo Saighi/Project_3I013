ffaee362a5b341ba6d6961464e959952903d646b
Votre Nom <paulsaighi@hotmail.com>
2019-03-27 00:14:07 +0100

    La fonction DistanceDeDamerauLevenshtein($prot1, $prot2) marche mais elle n'est pas encore incorporable au code , je m'en ocuppe soon

--- class/Orderer.php
+++ class/Orderer.php
@@ -48,40 +48,68 @@ class Orderer
             $i++;
         }
 
-        $da = array();
-        $ids1Length= count($ids1);
-        $ids2Length=count($ids2);
-
-        for ($i = 1; $i <= $sizeAlphabet; $i++) {
-            $da[$i] = 0;
-        }
+        $ids1Length = count($ids1);
+        $ids2Length = count($ids2);
 
         $d = array();
-        
-        for ($i=-1;$i<=$ids1Length;$i++){
-            $d[$i]= array();
+
+        for ($i = -1; $i <= $ids1Length; $i++) {
+            $d[$i] = array();
         }
-        
+
         $maxdist = $ids1Length + $ids2Length;
 
-        $d[-1][-1]= $maxdist;
-        
-        for ($i = 0; $i<$ids1Length;$i++){
+        $d[-1][-1] = $maxdist;
+
+        for ($i = 0; $i <= $ids1Length; $i++) {
             $d[$i][-1] = $maxdist;
             $d[$i][0] = $i;
         }
-        for ($i = 0; $i<$ids2Length;$i++){
+        for ($i = 0; $i <= $ids2Length; $i++) {
             $d[-1][$i] = $maxdist;
             $d[0][$i] = $i;
         }
 
-        for($i=1;$i<$ids1Length;$i++){
-            $db=0;
-            for($j=1;$j<$ids2Length;$j++){
+        $da = array();
+
+
+
+        for ($i = 1; $i <= $ids1Length; $i++) {
+            $db = 0;
+            for ($j = 1; $j <= $ids2Length; $j++) {
+
+                Orderer::af($d);
+                if (array_key_exists($ids2[$j],$da )) {
+                    $k = $da[$ids2[$j]];
+                } else {
+                    $k = 0;
+                }
+
+                $l=$db;
+
+                if ($ids1[$i]== $ids2[$j]){
+                    $cost=0;
+                    $db=$j;
+                }
+                else{
+                    $cost= 1;
+                }
+
+                $d[$i][$j]= min($d[$i-1][$j-1]+$cost,$d[$i][$j-1]+1,$d[$i-1][$j]+1, $d[$k-1][$l-1] + ($i-$k-1) + 1 + ($j-$l-1));
             }
+            $da[$ids1[$i]]=$i;
         }
 
-        return $d;
+        return $d[$ids1Length][$ids2Length];
+    }
+
+    static public function af($d){
+        foreach ($d as $k=>$v){
+            foreach ($v as $ke=>$va){
+                echo $va;
+            }
+            echo "</br>";
+        }
     }
 
     public function getOrderedListOfProteins()
--- class/testOrder.php
+++ class/testOrder.php
@@ -22,37 +22,26 @@ $c1 = new Domain(['id' => "c",
                         'confiance' => 0.3,
                         'first' => 2000,
                         'last' => 3000]);
-
-$a2 = new Domain(['id' => "a",
-                        'confiance' => 0.3,
-                        'first' => 500,
-                        'last' => 1000]);
-$b2 = new Domain(['id' => "b",
-                        'confiance' => 0.3,
-                        'first' => 0,
-                        'last' => 1]);
-
-$c2 = new Domain(['id' => "c",
+$V = new Domain(['id' => "V",
                         'confiance' => 0.3,
                         'first' => 2000,
                         'last' => 3000]);
 
-
 $prot1->addDomain($a1);
 $prot1->addDomain($b1);
 $prot1->addDomain($c1);
+$prot1->addDomain($V);
 
-$prot2->addDomain($a2);
-$prot2->addDomain($b2);
-$prot2->addDomain($c2);
+$prot2->addDomain($c1);
+$prot2->addDomain($V);
+$prot2->addDomain($b1);
+$prot2->addDomain($a1);
+
 
 $d = Orderer::DistanceDeDamerauLevenshtein($prot1,$prot2);
-foreach($d as $key=>$value){
-  foreach($value as $k=>$v){
-    echo $v;
-  }
-  echo "</br>";
-}
+
+echo $d;
+
 
 ?>
 
